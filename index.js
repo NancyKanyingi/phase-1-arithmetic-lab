9a0862c1eb5bb915321616a18657d77f58850475
Nancy <wangui.kanyingi2015@gmail.com>
2025-06-15 07:10:53 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let numOne = 31;
+let numTwo = 2;
+let multiply = numOne*numTwo
+    console.log(multiply)
 
-// Write your code here
+let random = Math.floor(Math.random() *100) +1;
+
+let num3 =25;
+let num4 =7;
+let mod = num3%num4
+console.log(mod)
+
+let max = Math.max(6,10,14,20);
+
+         
